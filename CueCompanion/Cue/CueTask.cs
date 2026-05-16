0765a96b2cced31c41b41bb990c5f8e03f5b03b6
Gergo Kaszas <gergo.kaszas@outlook.com>
2026-05-16 20:03:17 +1000

    Task icons

--- CueCompanion/Cue/CueTask.cs
+++ CueCompanion/Cue/CueTask.cs
@@ -1,3 +1,4 @@
+using MudBlazor;
 using SQLite;
 
 namespace CueCompanion;
@@ -18,4 +19,25 @@ public class CueTask
 
     [Column("tasks")]
     public string Tasks { get; set; } = "New task";
+
+    [Column("icon")]
+    public CueTaskIcon Icon { get; set; } = CueTaskIcon.Task;
+
+    public static string CueTaskIconToString(CueTaskIcon icon)
+    {
+        return icon switch
+               {
+                   CueTaskIcon.Task    => Icons.Material.Filled.Task,
+                   CueTaskIcon.Warning => Icons.Material.Filled.Warning,
+                   CueTaskIcon.Note    => Icons.Material.Filled.StickyNote2,
+                   _                   => Icons.Material.Filled.Task,
+               };
+    }
+}
+
+public enum CueTaskIcon
+{
+    Task    = 0,
+    Note    = 1,
+    Warning = 2,
 }
